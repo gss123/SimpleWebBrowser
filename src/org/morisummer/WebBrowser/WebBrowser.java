f55b78b7c0f25b618542783b827f319cdc2e8aff
Jonathan Munevar <jmunevar01@gmail.com>
2013-07-06 22:01:12 -0700

    Initial commit

---
+++ src/org/morisummer/WebBrowser/WebBrowser.java
@@ -0,0 +1,22 @@
+/*
+    Name: Jonathan Munevar
+    Class: CS 211D
+    Professor: A. Moghtanei
+    Date: 07/10/2013
+    Assignment: Assignment Four - Web Browser
+    Filename: WebBrowser.java
+*/
+
+package org.morisummer.WebBrowser;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class WebBrowser extends Activity
+{
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
